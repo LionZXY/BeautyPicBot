1a874684f786669db0961542d1d4f57262416006
Nikita Kulikov <lionzxy@yandex-team.ru>
2018-10-22 14:58:41 +0300

    Fix quotes

--- send_image.sh
+++ send_image.sh
@@ -1,12 +1,21 @@
+
 #!/usr/bin/env bash
 
 IMAGECOUNT=-1
 OUTFOLDER=./img
 source bot.conf
 
-PHOTOURL=$(curl -s "https://api.unsplash.com/photos/random?client_id=$CLIENT_ID" | jq -r '.urls.raw')
+echo $TELEGRAM_CHAT_ID
+
+PHOTOJSON=$(curl -s "https://api.unsplash.com/photos/random?client_id=$CLIENT_ID") 
+
+echo $PHOTOJSON
+
+PHOTOURL=$(echo $PHOTOJSON | jq -r '.urls.raw' | python3 -c "import urllib.parse; print(urllib.parse.quote_plus(input()))")
+
+echo $PHOTOURL
 
 for chat_id in ${TELEGRAM_CHAT_ID[*]}
 do
-    curl -s "https://api.telegram.org/bot${APITOKEN}/sendPhoto?chat_id=$chat_id&photo=$PHOTOURL"
+        curl -s "https://api.telegram.org/bot${APITOKEN}/sendPhoto?chat_id=$chat_id&photo=$PHOTOURL"
 done
